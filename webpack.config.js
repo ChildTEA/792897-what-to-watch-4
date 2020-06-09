46de4f2bced42c7a95e2268e18cbfba7da926ef8
Yaroslav Smirnov <yar14@mail.ru>
2020-06-09 10:58:31 +0300

    Build: Установлен Dev-server

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require(`path`);
+
+module.exports = {
+  entry: `./src/index.js`,
+  output: {
+    filename: `bundle.js`,
+    path: path.join(__dirname, `public`)
+  },
+  devServer: {
+    contentBase: path.join(__dirname, `public`),
+    open: true,
+		inline: false,
+    port: 1337,
+  },
+  devtool: `source-map`,
+};

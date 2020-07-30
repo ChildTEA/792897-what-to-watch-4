92e2e32d87713ad842ceb9f106ef4c804bf48e74
Yaroslav Smirnov <yar14@mail.ru>
2020-07-30 16:37:41 +0300

    Fix: Исправлены адреса изображений на страницах фильмов

--- src/components/movie-page-details/movie-page-details.jsx
+++ src/components/movie-page-details/movie-page-details.jsx
@@ -215,7 +215,7 @@ const MoviePageDetails = ({
           <div className="catalog__movies-list">
             <article className="small-movie-card catalog__movies-card">
               <div className="small-movie-card__image">
-                <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt="Fantastic Beasts: The Crimes of Grindelwald" width="280" height="175" />
+                <img src="/img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt="Fantastic Beasts: The Crimes of Grindelwald" width="280" height="175" />
               </div>
               <h3 className="small-movie-card__title">
                 <a className="small-movie-card__link" href="movie-page.html">Fantastic Beasts: The Crimes of Grindelwald</a>
@@ -224,7 +224,7 @@ const MoviePageDetails = ({
 
             <article className="small-movie-card catalog__movies-card">
               <div className="small-movie-card__image">
-                <img src="img/bohemian-rhapsody.jpg" alt="Bohemian Rhapsody" width="280" height="175" />
+                <img src="/img/bohemian-rhapsody.jpg" alt="Bohemian Rhapsody" width="280" height="175" />
               </div>
               <h3 className="small-movie-card__title">
                 <a className="small-movie-card__link" href="movie-page.html">Bohemian Rhapsody</a>
@@ -233,7 +233,7 @@ const MoviePageDetails = ({
 
             <article className="small-movie-card catalog__movies-card">
               <div className="small-movie-card__image">
-                <img src="img/macbeth.jpg" alt="Macbeth" width="280" height="175" />
+                <img src="/img/macbeth.jpg" alt="Macbeth" width="280" height="175" />
               </div>
               <h3 className="small-movie-card__title">
                 <a className="small-movie-card__link" href="movie-page.html">Macbeth</a>
@@ -242,7 +242,7 @@ const MoviePageDetails = ({
 
             <article className="small-movie-card catalog__movies-card">
               <div className="small-movie-card__image">
-                <img src="img/aviator.jpg" alt="Aviator" width="280" height="175" />
+                <img src="/img/aviator.jpg" alt="Aviator" width="280" height="175" />
               </div>
               <h3 className="small-movie-card__title">
                 <a className="small-movie-card__link" href="movie-page.html">Aviator</a>
